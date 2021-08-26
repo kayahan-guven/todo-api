dc9e54366ac0cf3f54997394528ef7e0ad3d9150
kayahan <kayahan@useinsider.com>
2021-08-26 15:56:42 +0300

    port change

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.use(express.json());
 app.use('/', indexRouter);
 app.use('/todos', todosRouter);
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log('listening on 3000')
 })
