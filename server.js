437047e1f3849d6b10a3ecf8f351f9d137d96e59
Tugay İlik <tugayilik@gmail.com>
2021-09-06 12:31:12 +0300

    Add cors middleware

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const { TextEncoder, TextDecoder } = require('util')
-global.TextEncoder = TextEncoder
-global.TextDecoder = TextDecoder
-const db = require('./helper/db')();
-require('dotenv').config()
+const { TextEncoder, TextDecoder } = require('util');
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+
+require('./helper/db')();
+require('dotenv').config();
+
 const indexRouter = require('./routes/index');
 const todosRouter = require('./routes/todos');
 const listsRouter = require('./routes/lists');
 
+app.use(cors());
 app.use(express.json());
 app.use('/', indexRouter);
 app.use('/todos', todosRouter);
 app.use('/lists', listsRouter);
 
-app.listen(process.env.PORT, function() {
-    console.log(`listening on ${process.env.PORT}`)
-})
+app.listen(process.env.PORT, function () {
+    console.log(`listening on ${process.env.PORT}`);
+});
